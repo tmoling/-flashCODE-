74234dcdce90ca1d6d2ba082da97a692491b7917
Troy Moling <troy.moling@gmail.com>
2018-03-19 17:06:02 -0500

    updated table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,32 @@
-INSERT INTO flashcards (questions, answers) 
-VALUES ('What does HTML stand for?', 'Hypertext Markup Language');
-
+INSERT INTO flashcards
+    (questions, answers)
+VALUES
+    ('What does HTML stand for?', 'Hypertext Markup Language');
 INSERT INTO flashcards
     (questions, answers)
 VALUES
     ('What does CSS stand for?', 'Cascading Style Sheets');
-
 INSERT INTO flashcards
     (questions, answers)
 VALUES
     ('What does API stand for?', 'Application Programming Interface');
+INSERT INTO flashcards
+    (questions, answers)
+VALUES
+    ('What is one way a website can store data in a browser?', 'Cookies');
+INSERT INTO flashcards
+    (questions, answers)
+VALUES
+    ('How many different values can booleans have?', 'Two (true and false)');
+INSERT INTO flashcards
+    (questions, answers)
+VALUES
+    ('What language are Express apps written in?', 'JavaScript');
+INSERT INTO flashcards
+    (questions, answers)
+VALUES
+    ('What does JSON stand for?', 'JavaScript Object Notation');
+INSERT INTO flashcards
+    (questions, answers)
+VALUES
+    ('What special character is used when coding with jQuery?', '$');
