ecaadf1433e0f732cd0b6ec87644feb1cafa7143
Jake <jaagaa@icloud.com>
2018-03-20 16:23:10 -0500

    :bug:

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -1,7 +1,8 @@
 var bcrypt = require('bcryptjs');
 var express = require('express');
 var router  = express.Router();
-var connection = require('../config/connection.js')
+var connection = require('../config/connection.js');
+var fs = require('fs');
 
 console.log("loaded quest contr");
 
@@ -10,59 +11,53 @@ console.log("loaded quest contr");
 router.get('/', function(req,res) {
 		//questNum is between 1 and 8
 		req.session.questNum = Math.floor(Math.random() * Math.floor(8)) + 1; //8 needs to be updated...
-		
+
 		var query = "SELECT questions, answers FROM flashcards WHERE id = ?"
 
 		connection.query(query, [req.session.questNum], function(err, result) {
+			if(err) throw err;
 			req.session.logged_in = true;
 
 		res.render('index', {
 				logged_in: req.session.logged_in,
 				answer: result[0].answers,
-				question: result[0].questions, //this is the randomly chosen question
+				question: result[0].questions,//this is the randomly chosen question
 				username: req.session.username
 		 });
+			console.log(result[0].answers);
+
+		
+
+			var append1 = fs.createWriteStream('log.txt', {
+				flags: 'a' // 'a' means appending (old data will be preserved)
+			})
+			var append2 = fs.createWriteStream('log.txt', {
+				flags: 'a' // 'a' means appending (old data will be preserved)
+			})
+
+			append1.write('\n\nQuestion: ' + JSON.stringify(result[0].questions));
+			append2.write('\nAnswer: ' + JSON.stringify(result[0].answers));
+
+			append1.end();
+			append2.end();
+
 	 });
 	 
  });
 
-// router.post('/submit', function(req,res){
-// 	 req.session.logged_in = true;
-// 	 var input = req.body.user_input;
-// 	 console.log(input);
 
-// 	 res.render('index',{
-// 		 input: input,
-// 		 logged_in: req.session.logged_in,
-// 		 answer: result[0].answers,
-// 		 question: result[0].questions, //this is the randomly chosen question
-// 		 username: req.session.username
-// 	 });
-
-//  });
+router.post('/submit', function(req,res){
+	 req.session.logged_in = true;
+	 
+		res.render('answer', {
+			logged_in: req.session.logged_in,
+			answer: req.session.res_answer,
+			question: req.session.res_question, //this is the randomly chosen question
+			input: req.body.user_input,
+			username: req.session.username
+	 });
+	console.log(req.body.user_input);
+ });
  
 
-
-
-//  router.post('/input', function(req,res){
-// 	 req.session.logged_in = true;
-
-// 		 if(req.session.user_input === '') {
-
-// 			res.render('index', {
-// 				logged_in: req.session.logged_in,
-// 				// questions: result[0].questions, //this is the randomly chosen question
-// 				username: req.session.username
-// 			});
-// 		 }
-// 		 res.render('index', {
-// 			logged_in: req.session.logged_in,
-// 			// questions: result[0].questions, //this is the randomly chosen question
-// 			input:res.session.user_input,	
-// 			// answer: result[0].answers,
-// 			username: req.session.username
-// 		 });
-		 
-		 
-//  });
 module.exports = router;
