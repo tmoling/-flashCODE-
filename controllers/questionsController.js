079736e80b897821a6993eea74ca56222d063956
Jake <jaagaa@icloud.com>
2018-03-20 14:09:42 -0500

    :bug:

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -10,19 +10,55 @@ console.log("loaded quest contr");
 router.get('/', function(req,res) {
 	console.log("hit questions path");
 	
-	var query = "SELECT questions FROM flashcards WHERE id = ?"
+	var query = "SELECT questions, answers FROM flashcards WHERE id = ?"
 
 	connection.query(query, [req.session.questNum], function(err, result) {
 		req.session.logged_in = true;
+
 	 res.render('index', {
-		 	logged_in: req.session.logged_in,
-			// id: result[0].id,
-			questions: result[0].questions,
-			answers:result[0].answers,
+			logged_in: req.session.logged_in,
+			answer: result[0].answers,
+			question: result[0].questions, //this is the randomly chosen question
 			username: req.session.username
 		 });
+		
 	 });
-   
+	 
+ });
+
+router.post('/submit', function(req,res){
+	 req.session.logged_in = true;
+	 var input = req.body.user_input;
+	 console.log(input);
+
+	 res.render('index',{
+		 input: input
+	 })
+
  });
  
+
+
+
+//  router.post('/input', function(req,res){
+// 	 req.session.logged_in = true;
+
+// 		 if(req.session.user_input === '') {
+
+// 			res.render('index', {
+// 				logged_in: req.session.logged_in,
+// 				// questions: result[0].questions, //this is the randomly chosen question
+// 				username: req.session.username
+// 			});
+// 		 }
+// 		 res.render('index', {
+// 			logged_in: req.session.logged_in,
+// 			// questions: result[0].questions, //this is the randomly chosen question
+// 			input:res.session.user_input,	
+// 			// answer: result[0].answers,
+// 			username: req.session.username
+// 		 });
+		 
+		 
+//  });
 module.exports = router;
